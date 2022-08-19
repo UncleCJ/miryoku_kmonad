177e3c562a8dc3ce65d724d7e1838d3d674ad5c3
CJ Sveningsson <cj.sveningsson@gmail.com>
2022-08-19 15:23:23 +0200

    Custom rules for Linux QWERTY

--- src/custom_rules.mk
+++ src/custom_rules.mk
@@ -1,3 +1,7 @@
 # Copyright 2021 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+MIRYOKU_ALPHAS=QWERTY
+MIRYOKU_NAV=VI
+MIRYOKU_MAPPING=LITE
+MIRYOKU_KMONAD_KEYBOARD_LINUX=/dev/input/event3
